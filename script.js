c45b6269fa0ed3f1870f8babe3636c360a62c690
Kaushal-Chaudhary56 <kaushalchaudhary897@gmail.com>
2023-08-03 09:27:04 +0545

    css completed started javascript

---
+++ script.js
@@ -0,0 +1,26 @@
+// console.log('i m kushal chaudhary ')
+// console.log('kaushal Chaudhary student of web designing')
+
+
+
+// alert("hello ")
+
+
+// js 3 tpes variable declare =>{
+//     let 
+//     const 
+//     var
+// }
+// const  let 
+// {
+
+// }
+
+
+
+// var a= (' kaushal Chaudhary')
+// var b= (' Welcome here ')
+// alert ( a + b )
+
+var a ="kushal chaudhary "
+prompt (a)
